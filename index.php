1528f3bc6d72120c1c862f9884fe1d34fa2a3f3b
Muhammad Ahsan Khan <56512649+mahsankhaan@users.noreply.github.com>
2019-12-20 19:51:51 +0500

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ body, html {
 ​
 <div class="bg"></div>
 ​
-<p>This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.</p>
+<p>Welcome Everyone!</p>
 ​
 </body>
 </html>
